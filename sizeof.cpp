211e7441de667213824c7d85d4dd7357e9cf3ca3
amanchauhan47 <amanchauhan0047@gmail.com>
2023-08-10 22:17:12 +0530

    added some cool stuff

---
+++ sizeof.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //int a;
+    float b;
+    char c;
+    bool d;
+
+    cout << "Size of int is "  << sizeof(int)  << endl;
+    cout << "Size of float is "<< sizeof(float)<< endl;
+    cout << "Size of char is " << sizeof(char) << endl;
+    cout << "Size of bool is " << sizeof(bool) << endl;
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;      //declaration
+    int b = 4;  //initialisation
+
+    cout << "b = " << b << endl;
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    cout << "a = " << a << endl;
+
+}
