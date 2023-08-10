9e249d454a5549e1cf5a47f37da61a0d4d478804
amanchauhan47 <amanchauhan0047@gmail.com>
2023-08-10 17:54:12 +0530

    My first C++ Program

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std; //we can add this stuff.
+int main()
+{
+    cout << "Hello World\n";
+
+    //std:count << "Hello World\n" instead of doing this again n again.
+    return 0;
+}
