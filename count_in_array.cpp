0c3dacfbbab84f5c7c6712377332f8768181a557
amanchauhan47 <amanchauhan0047@gmail.com>
2023-08-17 17:37:28 +0530

    added

---
+++ count_in_array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//              to be continued.
+int main()
+{
+    int array[5] = {1,1,2,3,5};
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            //if()
+        }
+    }
+}
