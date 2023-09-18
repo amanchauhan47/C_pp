b8e37e0a0a1449da5376552f98240d00d5447460
amanchauhan47 <amanchauhan0047@gmail.com>
2023-09-18 16:09:37 +0530

    added

---
+++ structure.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+struct Student{
+    char name[100];
+    int roll;
+    float cgpa;
+}stu;               //In C++ we can create aliases without using typedef keyword;
+int main()
+{
+    stu s1;
+}
