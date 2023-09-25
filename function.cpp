b0cde8e618bbf9a52f202859133ff9c717fbe1f1
amanchauhan47 <amanchauhan0047@gmail.com>
2023-09-25 23:33:35 +0530

    factorial function

---
+++ function.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int fact(int n);
+int main()
+{
+    int num;
+    int num2;
+    int f;
+    cout << "Enter the number: ";
+    cin >> num;
+    cin >> num2;
+    cout << fact(num) << endl;
+    cout << fact(num2) << endl;
+    return 0;
+}
+int fact(int n)
+{   
+    int f=1;
+    for(int i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    return f;
+}
