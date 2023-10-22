df8b7b3f6ea7e5a462c7100c855432b1a3de3340
amanchauhan47 <amanchauhan0047@gmail.com>
2023-10-22 13:57:27 +0530

    added

--- binary_search2.cpp
+++ binary_search2.cpp
@@ -1,43 +1,37 @@
-#include<iostream>
+#include<iostream>//                       Binary Search (Self Created)..
 using namespace std;
-int main()
-{
-    int n;
-    int s,e;
+int binarySearch(int array[], int n, int key){
+    int s = 0;
+    int e = n-1;
+    while(s<=e){
+        int i = (s+e)/2;
 
-    // cout << "Enter the size of array: ";
-    // cin >> n;
-    // int array[n];
-    // cout << "Enter the elements of array: ";
-    // for(int i=0;i<n;i++){
-    //     cin >> array[i];
-    // }
-    int array[10] = {1,5,10,11,21,27,54,69,71,89};
-    n = 10;
-
-    int key;
-    cout << "Enter the key u wanna search: ";
-    cin >> key;
-    s = 0;
-    e = n-1;
-
-    for(int i=(s+e)/2;i<n;i=(s+e)/2){
-        cout << "a" << endl;
         if(array[i] == key){
-            cout << "\nKey found at index " << i << endl;
-            break;
-        }
-        else if(s>e){
-            cout << "\nKey not found.\n";
-            break;
+            return i;
         }
         else if(array[i]>key){
-            cout << "a" << endl;
             e = i-1;
         }
         else{
             s = i+1;
         }
     }
+    return -1;
+}
+int main()
+{
+    int n, key;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    int array[n];
+    cout << "Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin >> array[i];
+    }
+    cout << "Enter the key u wanna search: ";
+    cin >> key;
+
+    cout << binarySearch(array,n,key) << endl;
     return 0;
 }
