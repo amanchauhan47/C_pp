5d1f505ea643084b2f9a14c51fc603a46157b2c7
amanchauhan47 <amanchauhan0047@gmail.com>
2023-11-21 20:14:03 +0530

    added

--- DSA/array_adt_using_class.cpp
+++ DSA/array_adt_using_class.cpp
@@ -19,6 +19,8 @@ void append(class Array *arr, int n);
 void remove(class Array *arr, int x);
 int Min(class Array arr);
 int Max(class Array arr);
+void reverse(class Array arr);
+int sum(class Array arr,int n);
 
 int main()
 {
@@ -38,15 +40,16 @@ int main()
     }
     //replace(arr,1,47);
     //cout << LinearSearch(arr,10) << endl;
-    cout << BinarySearch(arr,8) << endl;
+    //cout << BinarySearch(arr,8) << endl;
     //get(arr,0);
     //insert(&arr,14,5);
     //reversePrint(arr);
+    //cout << sum(arr,arr.length-1) << endl;
     //append(&arr,47);
     //remove(&arr,4);
     //cout << Min(arr) << endl;
     //cout << Max(arr) << endl;
-
+    reverse(arr);
     display(arr);
     return 0;
 }
@@ -64,6 +67,24 @@ int LinearSearch(class Array arr, int n){
         }   
     }
     return -1;
+}
+int sum(class Array arr, int n){            //How this is working...
+    if(n<0){
+        return 0;
+    }
+    else{
+        return sum(arr,n-1) + arr.ptr[n];
+    }
+}
+void reverse(class Array arr){
+    int rev[arr.length];
+    for(int i=arr.length-1,j=0; i>=0; i--,j++)
+    {
+        rev[j] = arr.ptr[i];
+    }
+    for(int i=0;i<5;i++){
+        arr.ptr[i] = rev[i];
+    }
 }
 int BinarySearch(class Array arr, int key){
     int i,a,b;
@@ -85,10 +106,14 @@ int BinarySearch(class Array arr, int key){
     return -1;
 }
 void replace(class Array arr, int index, int n){
+    if(index >= 0 && index < arr.length){
     arr.ptr[index] = n;
+    }
 }
 void get(class Array arr, int index){
+    if(index >= 0 && index < arr.length){
     cout << arr.ptr[index] << " is at index " << index << endl;
+    }
 }
 void insert(class Array *arr, int n, int index){
     if((index >= 0 && index <= arr->length) && (arr->size > arr->length)){ // and also check index should lie between 0 to arr.length
---
+++ DSA/reversing_an_array.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void RevMethod1(int arr[], int n);
+void RevMethod2(int arr[], int n);
+void display(int arr[], int n);
+void swap(int *a, int *b);
+int main(){
+    int arr[5] = {1,2,47,4,5};
+    //RevMethod1(arr,5);
+    RevMethod2(arr,5);
+    display(arr,5);
+    
+    return 0;
+}
+void RevMethod1(int arr[], int n){
+    int j;
+    int rev[n];
+    for(int i=n-1,j=0; i>=0; i--,j++)
+    {
+        rev[j] = arr[i];
+    }
+    for(int i=0;i<n;i++){
+        arr[i] = rev[i];
+    }
+}
+void RevMethod2(int arr[], int n){
+    int j=n-1;
+    for(int i=0;i<n/2;i++){
+        swap(arr[i], arr[j]);
+        j--;
+    }
+}
+void display(int arr[], int n){
+    for(int i=0;i<n;i++)
+    {
+        cout << arr[i] << "\t";
+    }
+}
+void swap(int *a, int *b){
+    int c;
+    c = *a;
+    *a = *b;
+    *b = c;
+}
