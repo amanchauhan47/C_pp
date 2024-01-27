1d698fce8b989f6786ad4634940091640ff01dc2
amanchauhan47 <amanchauhan0047@gmail.com>
2024-01-27 17:04:26 +0530

    added

--- errors.cpp
+++ errors.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     float f = 4.0;
-    f = 4.0%2.0;
+    //f = 4.0f%2.0f;  //That's because the modulus operator % cannot apply to float or double. It's meant to get the remainder when integer type x is divided by y. It does not have any meaning when you use it with float or double.
     cout << f << endl;
 
     return 0;
